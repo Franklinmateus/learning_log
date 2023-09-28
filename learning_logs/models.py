c6e9f168781e552d81eac140a0b7261391e74c6b
Franklinmateus <frankmateus660@gmail.com>
2023-09-28 16:59:41 -0300

    Adicionando primeiros models

---
+++ learning_logs/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text 
+    
+class Entry(models.Model):
+    """Algo específico sobre um tópico"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """Devolve a string do modelo"""
+        return self.text[:50] + "..."    
